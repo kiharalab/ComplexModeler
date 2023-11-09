3210f25045243b3ca1d881a3f00c8cf41e15cc97
Xiao Wang <wang3702@purdue.edu>
2023-11-09 13:37:31 -0500

    update set up configuration

--- set_up.sh
+++ set_up.sh
@@ -1,4 +1,5 @@
 mkdir example
+cd example
 wget https://kiharalab.org/emsuites/complexmodeler/input/20031.mrc
 wget https://kiharalab.org/emsuites/complexmodeler/input/20031.fasta
 cd ..
@@ -7,10 +8,9 @@ mkdir DiffModeler/data
 cd DiffModeler/best_model
 wget https://huggingface.co/zhtronics/DiffModelerWeight/resolve/main/diffusion_best.pth.tar
 cd ..
-cd data
 wget https://huggingface.co/datasets/zhtronics/BLAST_RCSB_AFDB/resolve/main/data.tar.gz.aa
 wget https://huggingface.co/datasets/zhtronics/BLAST_RCSB_AFDB/resolve/main/data.tar.gz.ab
 wget https://huggingface.co/datasets/zhtronics/BLAST_RCSB_AFDB/resolve/main/data.tar.gz.ac
 cat data.tar.gz.aa data.tar.gz.ab data.tar.gz.ac >data.tar.gz
 tar -xzvf data.tar.gz
-cd ../..
+cd ..
